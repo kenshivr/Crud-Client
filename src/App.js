e3bad12f1f368f895ce536f5d2502532716bb3f7
Kenshi <Aaladar@proton.me>
2024-01-11 21:41:34 -0600

    Se agregan los useState y se crea la funcion mostrarDatos

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [nombre,setNombre] = useState("");
+  const [edad,setEdad] = useState(0);
+  const [pais,setPais] = useState("");
+  const [cargo,setCargo] = useState("");
+  const [anios,setAnios] = useState(0);
+
+  const mostrarDatos = () => {
+    alert(nombre);
+  };
+
   return (
     <div className="App">
+
+      <div className="datos">
+
+        <label>Nombre: <input 
+        onChange={(event) => {
+          setNombre(event.target.value);
+        }}
+        type="text"/></label>
+
+        <label>Edad: <input 
+        onChange={(event) => {
+          setEdad(event.target.value);
+        }}
+        type="number"></input></label>
+
+        <label>Pais: <input 
+        onChange={(event) => {
+          setPais(event.target.value);
+        }}
+        type="text"></input></label>
+
+        <label>Cargo: <input 
+        onChange={(event) => {
+          setCargo(event.target.value);
+        }}
+        type="text"></input></label>
+
+        <label>Años: <input 
+        onChange={(event) => {
+          setAnios(event.target.value);
+        }}
+        type="number"></input></label>
+
+        <button onClick={mostrarDatos} >Registrar</button>
+
+      </div>
 
     </div>
   );
